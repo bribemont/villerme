bff642d4568a1156b93c5072c21783a3f9fa950b
bribemont <contact@pleir-mv.net>
2014-01-18 12:56:15 +0100

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,7 @@ Villerme::Application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
+    enable_starttls_auto: false,
     address: "localhost",
     port: 25
   }
