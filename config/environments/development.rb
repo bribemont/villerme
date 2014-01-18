d0056d68f54e79ddd3ec7cc3457a65f82489aabd
bribemont <contact@pleir-mv.net>
2014-01-18 11:51:34 +0100

    ajout smtp linode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,13 +18,8 @@ Villerme::Application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: "max.pleir-mv.net",
-    port: 25,
-    domain: "pleir-mv.net",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: nil,
-    password: nil
+    address: "localhost",
+    port: 25
   }
 
   # Print deprecation notices to the Rails logger
